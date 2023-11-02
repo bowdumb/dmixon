77acb0bbd4a6552912e7eb191159fa767c9e0b01
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-11-02 12:12:28 -0400

    Corrected issues with CSS on welcome page.

--- dmixon-business/src/components/Welcome.js
+++ dmixon-business/src/components/Welcome.js
@@ -1,15 +1,66 @@
 import React from 'react';
+import '../styles/Welcome.css';
 
 function Welcome() {
     return (
-        <div className="welcome-content">
-            <p>
-                Welcome to Danielle's website! You can't see it, but she's got a great can!
-
-                We'll have a welcome letter from Danielle here.
-            </p>
+        <div className="welcome-container">
+            <div className="welcome-image">
+                <img src="assets/placeholder.jpeg" alt="A photo of Danielle" />
+            </div>
+            <div className="letter-container">
+                <p>
+                    Welcome to Danielle's website! You can't see it, but she's got a great can!
+                    We'll have a welcome letter from Danielle here.
+                </p>
+                <p>
+                    Danielle is great. She's very intelligent, she's great with  kids, and she's got a cool accent.
+                </p>
+                <p>
+                    I like Danielle a lot, and I'm sure that you will too!
+                </p>
+                <p>
+                    Welcome to Danielle's website! You can't see it, but she's got a great can!
+                    We'll have a welcome letter from Danielle here.
+                </p>
+                <p>
+                    Danielle is great. She's very intelligent, she's great with  kids, and she's got a cool accent.
+                </p>
+                <p>
+                    I like Danielle a lot, and I'm sure that you will too!
+                </p>
+                <p>
+                    Welcome to Danielle's website! You can't see it, but she's got a great can!
+                    We'll have a welcome letter from Danielle here.
+                </p>
+                <p>
+                    Danielle is great. She's very intelligent, she's great with  kids, and she's got a cool accent.
+                </p>
+                <p>
+                    I like Danielle a lot, and I'm sure that you will too!
+                </p>
+                <p>
+                    Welcome to Danielle's website! You can't see it, but she's got a great can!
+                    We'll have a welcome letter from Danielle here.
+                </p>
+                <p>
+                    Danielle is great. She's very intelligent, she's great with  kids, and she's got a cool accent.
+                </p>
+                <p>
+                    I like Danielle a lot, and I'm sure that you will too!
+                </p>
+                <p>
+                    Welcome to Danielle's website! You can't see it, but she's got a great can!
+                    We'll have a welcome letter from Danielle here.
+                </p>
+                <p>
+                    Danielle is great. She's very intelligent, she's great with  kids, and she's got a cool accent.
+                </p>
+                <p>
+                    I like Danielle a lot, and I'm sure that you will too!
+                </p>
+            </div>
         </div>
     );
-};
+}
 
 export default Welcome;
