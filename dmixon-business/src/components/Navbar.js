c342f2d768ad0a8587d793e25bbfef30f70a5a85
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-07-11 23:29:42 -0400

    Added activeLink functionality so that the navbar communicates which page you have navigated to.

--- dmixon-business/src/components/Navbar.js
+++ dmixon-business/src/components/Navbar.js
@@ -26,12 +26,18 @@ function Navbar() {
           <nav>
             <div className="navbar-links">
               <div className="spacer"></div>
-              <NavLink to="/" exact={true} className="welcome-link">Welcome</NavLink>
+              <NavLink
+                to="/"
+                exact
+                className={`welcome-link ${activeLink === '/' ? 'active-link' : ''}`}
+                onClick={() => handleLinkClick('/')}
+              >
+                Welcome
+              </NavLink>
               <NavLink
                 to="/about"
-                className="nav-link"
-                activeClassName="active-link"
-                onClick={() => handleLinkClick("/about")}
+                className={`nav-link ${activeLink === '/about' ? 'active-link' : ''}`}
+                onClick={() => handleLinkClick('/about')}
               >
                 About Danielle
               </NavLink>
@@ -42,41 +48,61 @@ function Navbar() {
               >
                 <NavLink
                   to="/individuals"
-                  className="nav-link"
-                  activeClassName="active-link"
-                  onClick={() => handleLinkClick("/individuals")}
+                  className={`nav-link ${activeLink === '/individuals' ? 'active-link' : ''}`}
+                  onClick={() => handleLinkClick('/individuals')}
                 >
                   Services
                 </NavLink>
                 {dropdownVisible && (
                   <ul className="dropdown">
-                    <li className="dropdown-item"><NavLink to="/individuals/service1" className="dropdown-link">Trauma Therapy For Individuals</NavLink></li>
-                    <li className="dropdown-item"><NavLink to="/individuals/service2" className="dropdown-link">Clinical Supervision For Social Workers</NavLink></li>
-                    <li className="dropdown-item"><NavLink to="/individuals/service3" className="dropdown-link">Consulting For Businesses/Communities</NavLink></li>
+                    <li className="dropdown-item">
+                      <NavLink
+                        to="/individuals/service1"
+                        className={`dropdown-link ${activeLink === '/individuals/service1' ? 'active-link' : ''}`}
+                        onClick={() => handleLinkClick('/individuals/service1')}
+                      >
+                        Trauma Therapy For Individuals
+                      </NavLink>
+                    </li>
+                    <li className="dropdown-item">
+                      <NavLink
+                        to="/individuals/service2"
+                        className={`dropdown-link ${activeLink === '/individuals/service2' ? 'active-link' : ''}`}
+                        onClick={() => handleLinkClick('/individuals/service2')}
+                      >
+                        Clinical Supervision For Social Workers
+                      </NavLink>
+                    </li>
+                    <li className="dropdown-item">
+                      <NavLink
+                        to="/individuals/service3"
+                        className={`dropdown-link ${activeLink === '/individuals/service3' ? 'active-link' : ''}`}
+                        onClick={() => handleLinkClick('/individuals/service3')}
+                      >
+                        Consulting For Businesses/Communities
+                      </NavLink>
+                    </li>
                   </ul>
                 )}
               </div>
               <NavLink
                 to="/organizations"
-                className="nav-link"
-                activeClassName="active-link"
-                onClick={() => handleLinkClick("/organizations")}
+                className={`nav-link ${activeLink === '/organizations' ? 'active-link' : ''}`}
+                onClick={() => handleLinkClick('/organizations')}
               >
                 For Organizations
               </NavLink>
               <NavLink
                 to="/resources"
-                className="nav-link"
-                activeClassName="active-link"
-                onClick={() => handleLinkClick("/resources")}
+                className={`nav-link ${activeLink === '/resources' ? 'active-link' : ''}`}
+                onClick={() => handleLinkClick('/resources')}
               >
                 Resources
               </NavLink>
               <NavLink
                 to="/contact"
-                className="nav-link-contact"
-                activeClassName="active-link"
-                onClick={() => handleLinkClick("/contact")}
+                className={`nav-link-contact ${activeLink === '/contact' ? 'active-link' : ''}`}
+                onClick={() => handleLinkClick('/contact')}
               >
                 Contact
               </NavLink>
