5ce89a070332f38261eeca26ad87d87d18171591
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-07-23 10:15:53 -0400

    Reworked the People page into Danielle's bio/headshot and created new blank People page to direct to staff member's individual pages.

--- dmixon-business/src/components/People.js
+++ dmixon-business/src/components/People.js
@@ -4,30 +4,8 @@ import BodyContent from './BodyContent';
 function People () {
     return (
         <BodyContent>
-        <div className = "about-content">
-            <h1 className = "about-title">Danielle Mixon, LCSW (with headshot)</h1>
-            <p>
-                I founded Haven Trauma Therapy and Consulting because I am passionate about offering resources that provide relief for people who have experienced trauma.
-            </p>
-            <p>
-                I am a clinical social worker, licensed in the state of New York, with more than a decade of experience in trauma work. I have worked with hundreds of
-                individuals across diverse communities. I consider providing appropriate care for trauma survivors as they courageously tend to the impact of their 
-                most painful experiences as one of my greatest responsibilities and privileges. The lessons I have learned from others’ willingness to be vulnerable 
-                with me are as influential to my approach as my formal education.
-            </p>
-            <p>
-                After earning an MSW from Columbia University, I obtained a certificate in Integrated Trauma Studies by completing a two-year program at the Institute 
-                for Contemporary Psychotherapy. I am certified in Eye Movement Desensitization and Reprocessing (EMDR) which is one therapeutic intervention designed to 
-                alleviate the distress associated with traumatic memories.
-            </p>
-            <p>
-                In addition to providing services to individuals, I have designed, developed, and managed community-based therapeutic and social-emotional programs. Also, 
-                as a director-level consultant, I led project teams working with client organizations seeking to improve their community’s ability to understand, prevent, 
-                or respond to sexual harassment and abuse.
-            </p>
-        </div>
-        </BodyContent>
 
+        </BodyContent>
     );
 };
 
