6baff1bbe3381c44cf6a5ba7ba9cb202e1939fdf
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-07-23 10:37:13 -0400

    Added placeholder image for bio headshots on People page and created oval shape to headshot containers.

--- dmixon-business/src/components/People.js
+++ dmixon-business/src/components/People.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../styles/People.css';
 import BodyContent from './BodyContent';
+
 
 function People () {
     return (
         <BodyContent>
+            <div className="danielle-bio-container">
+                <div className="danielle-bio-headshot">
+                    <img src="assets/placeholder.jpeg" alt="Danielle's headshot" className="people-headshots" />
+                </div>
+            </div>
 
         </BodyContent>
     );
