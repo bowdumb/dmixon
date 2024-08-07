7f50501a005c9b98c7cab7de3ec4f4eb8d2cd430
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-08-07 11:39:27 -0400

    Removed placeholder image from Welcome page, reworked the containers on the page, and added font support.

--- dmixon-business/src/components/Welcome.js
+++ dmixon-business/src/components/Welcome.js
@@ -1,36 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Welcome.css';
 import BodyContent from './BodyContent';
 
 function Welcome() {
     return (
         <BodyContent>
-        <div className="welcome-container">
-            <div className="welcome-image">
-                <img src="assets/placeholder.jpeg" alt="Danielle's headshot" />
+            <div className="welcome-container">
+                <div className="welcome-header">
+                    Trauma-centered therapy for individuals, consulting for businesses, and  
+                    clinical supervision for therapists
+                </div>
+                <div className="contact-button-welcome">
+                <Link to="/contact">Book a session</Link>
+                </div>
+                <div className="letter-container">
+                    <p>
+                        Haven provides resources for adults who are navigating life after trauma and leaders who are creating more
+                        trauma-informed environments. Every aspect of the services we provide and the way we provide them are designed
+                        to be antidotes to the negative effects of traumatic experiences.
+                    </p>
+                    <p>
+                        We offer:
+                    </p>
+                    <ul>
+                        <li>Individual teletherapy for adults in NY who have experienced trauma</li>
+                        <li>Online clinical supervision for social workers who specialize in trauma work</li>
+                        <li>Consulting and educational services for organizations who are building trauma-informed systems </li>
+                    </ul>
+                    <p>
+                        We prioritize integrity and inclusivity in the Haven community. A cornerstone of our work in the trauma field
+                        is recognizing that oppressive systems and exploitation of power are pervasive in our society. We aim to operate
+                        in a way that counters powerlessness and dehumanization, radically welcomes people from marginalized communities,
+                        and provides appropriate and nonjudgmental support to all.
+                    </p>
+                </div>
             </div>
-            <div className="letter-container">
-                <p>
-                Haven provides resources for adults who are navigating life after trauma and leaders who are creating more
-                 trauma-informed environments. Every aspect of the services we provide and the way we provide them are designed
-                  to be antidotes to the negative effects of traumatic experiences. 
-                </p>
-                <p>
-                    We offer:
-                </p>
-                <ul>
-                <li>Individual teletherapy for adults in NY who have experienced trauma</li>
-                <li>Online clinical supervision for social workers who specialize in trauma work</li>
-                <li>Consulting and educational services for organizations who are building trauma-informed systems </li>
-                </ul>
-                <p>
-                We prioritize integrity and inclusivity in the Haven community. A cornerstone of our work in the trauma field
-                 is recognizing that oppressive systems and exploitation of power are pervasive in our society. We aim to operate
-                  in a way that counters powerlessness and dehumanization, radically welcomes people from marginalized communities,
-                   and provides appropriate and nonjudgmental support to all.
-                </p>
-            </div>
-        </div>
         </BodyContent>
     );
 }
