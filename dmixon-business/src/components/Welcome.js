2ac5dfb5fc6394e484e424ff3ec6df64a3e49b82
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-08-08 00:25:44 -0400

    Added separate containers for segments of text to display mission statement on Welcome page.

--- dmixon-business/src/components/Welcome.js
+++ dmixon-business/src/components/Welcome.js
@@ -8,32 +8,33 @@ function Welcome() {
         <BodyContent>
             <div className="welcome-container">
                 <div className="welcome-header">
-                    Trauma-centered therapy for individuals, consulting for businesses, and  
+                    Trauma-centered therapy for individuals, consulting for businesses, and
                     clinical supervision for therapists
                     <div className="contact-button-welcome">
-                        <Link to="/contact" className="book-session-button">Book a session</Link>
+                        <Link to="/contact" className="book-session-button">Book a consultation</Link>
                     </div>
                 </div>
                 <div className="letter-container">
-                    <p>
+                    <div className="statement-1">
                         Haven provides resources for adults who are navigating life after trauma and leaders who are creating more
                         trauma-informed environments. Every aspect of the services we provide and the way we provide them are designed
                         to be antidotes to the negative effects of traumatic experiences.
-                    </p>
-                    <p>
-                        We offer:
-                    </p>
-                    <ul>
-                        <li>Individual teletherapy for adults in NY who have experienced trauma</li>
-                        <li>Online clinical supervision for social workers who specialize in trauma work</li>
-                        <li>Consulting and educational services for organizations who are building trauma-informed systems </li>
-                    </ul>
-                    <p>
+                    </div>
+                    <div className="statement-2">
+                        <p>We offer:</p>
+
+                        <ul>
+                            <li>Individual teletherapy for adults in NY who have experienced trauma</li>
+                            <li>Online clinical supervision for social workers who specialize in trauma work</li>
+                            <li>Consulting and educational services for organizations who are building trauma-informed systems </li>
+                        </ul>
+                    </div>
+                    <div className="statement-3">
                         We prioritize integrity and inclusivity in the Haven community. A cornerstone of our work in the trauma field
                         is recognizing that oppressive systems and exploitation of power are pervasive in our society. We aim to operate
                         in a way that counters powerlessness and dehumanization, radically welcomes people from marginalized communities,
                         and provides appropriate and nonjudgmental support to all.
-                    </p>
+                    </div>
                 </div>
             </div>
         </BodyContent>
